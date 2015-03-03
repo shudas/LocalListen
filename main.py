cadc4832a5c1304a7a169961d035025575f00585
Shu <shudas@umich.edu>
2015-03-03 01:06:42 -0600

    redirect after login

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 __author__ = 'Shu'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    SECRET_KEY='development key'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        app.logger.debug(request.data)
+        print(request.data)
+        session['access_token'] = request
+        session.modified = True
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
