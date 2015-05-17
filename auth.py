4521b42e7044c594d33d3f32ba5a73da7dbeea28
Shu <shudas@umich.edu>
2015-05-17 02:03:34 -0500

    use index and redirect to listen on success fb

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ fb_app_id = '617235401742568'
 fb_app_secret = '1f3a8f0428a1b79265f10af6863a43c2'
 
 
-def valid_token(token):
+def valid_token(token, uid):
     """Determine if the given user token is valid"""
     app_access = fb_app_id + '|' + fb_app_secret
     args = {'input_token': token, 'access_token': app_access}
@@ -17,9 +17,9 @@ def valid_token(token):
     data = r.json()
     main.app.logger.debug('valid token returned: ')
     main.app.logger.debug(data)
-    if data['data']['is_valid']:
+    if (data['data']['is_valid'] and
+            str(data['data']['user_id']) == str(uid) and
+            str(data['data']['app_id']) == str(fb_app_id)):
         return True
     else:   # How to handle invalid access token?
         return False
-
-
--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 __author__ = 'Shu'
 """Routing"""
 import auth
-from flask import Flask, render_template, request, session
-app = ""
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = "MY SECRET KEY WHICH SHOULD BE CHANGED"
 app.debug = True
 
 
-@app.route('/', methods=['GET', 'POST'])
+# redirects to /listen on success
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html', redir="/listen")
+
+@app.route('/listen', methods=['GET', 'POST'])
+def listen():
     if request.method == 'POST':
         try:
             token = request.form['accessToken']
+            uid = request.form['userID']
             app.logger.debug('token from POST: ' + token)
-            if auth.valid_token(token):
+            if auth.valid_token(token, uid):
                 session['access_token'] = token
+                session['user_id'] = uid
                 session['logged_in'] = True
-                return render_template('index.html', logged_in='True')
+                return render_template('listen.html')
         except Exception:
             app.logger.debug("Could not parse the POST params properly")
-            return render_template('index.html', logged_in='False')
+            return redirect(url_for('index', redir="/listen"))
     elif session.get('logged_in'):
-        return render_template('index.html', logged_in='True')
-    app.logger.debug('GOT BEFORE RENDER INDEX ELSE')
-    return render_template('index.html', logged_in='False')
+        return render_template('listen.html')
+    # by default, go to index in case not logged in
+    return redirect(url_for('index', redir="/listen"))
 
 if __name__ == '__main__':
     app.run()
