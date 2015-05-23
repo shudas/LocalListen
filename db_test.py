f3da501e6cd9389fc447121bc9f033118f63c787
Shu <shudas@umich.edu>
2015-05-23 03:48:37 -0500

    database testing works

--- db_test.py
+++ db_test.py
@@ -1,9 +1,32 @@
 import mysql.connector as connector
+import config_parser
+from config_parser import config as conf
+from mysql.connector import errorcode
 
-cnx = connector.connect(user='root', password='mysqlpass',
-                        host='localhost',
-                        database='locallisten')
+TABLES = {}
+TABLES['genres'] = "CREATE TABLE IF NOT EXISTS Genres (genre VARCHAR(50) PRIMARY KEY);"
 
+def run_db():
+    cnx = connector.connect(**conf['DB'])
+    cursor = cnx.cursor()
 
+    print TABLES
 
-cnx.close()
+    for name, ddl in TABLES.iteritems():
+        try:
+            print("Creating table {}: ".format(name))
+            cursor.execute(ddl)
+        except connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+    cursor.close()
+    cnx.close()
+
+if __name__ == '__main__':
+    config_parser.read_config()
+    run_db()
